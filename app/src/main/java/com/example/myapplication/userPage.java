96b84f82634c4781c20091deb0512aceda1965b9
ofekyariv <ofekyariv28@gmail.com>
2021-06-01 19:13:32 +0300

    1

--- app/src/main/java/com/example/myapplication/userPage.java
+++ app/src/main/java/com/example/myapplication/userPage.java
@@ -6,15 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class userPage extends AppCompatActivity implements View.OnClickListener {
     Button search,create,live;
     TextView tv_contact,tv_about;
+    private static final int VIDEO_CAPTURE = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,8 +51,8 @@ public class userPage extends AppCompatActivity implements View.OnClickListener 
             startActivity(intent);
         }
         if (v == live) {
-            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-            startActivity(intent);
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            startActivityForResult(intent, VIDEO_CAPTURE);
         }
         if (v == tv_contact) {
             Intent intent = new Intent(this, Contact.class);
@@ -59,6 +63,24 @@ public class userPage extends AppCompatActivity implements View.OnClickListener 
             startActivity(intent);
         }
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri videoUri = data.getData();
+
+        if (requestCode == VIDEO_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video saved to:\n" +
+                        videoUri, Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) { // Add menu
